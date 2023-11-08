0ef232bd68564f1011d1ba42c557c3d30afbff9e
Yash <patilpyash@gmail.com>
2023-11-09 03:29:03 +0530

    forgot password implemented

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt'); // Adding bcrypt for password hashing
 const { swaggerUi, swaggerSpec } = require('./swagger'); // Import your Swagger configuration
+const crypto = require('crypto'); // Import the crypto module for generating tokens
+
 
 
 // create connection
@@ -77,6 +79,119 @@ app.post('/register', (req, res) => {
         });
     });
 });
+
+app.post('/forgot-password', (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ msg: 'Please enter your email address' });
+    }
+
+    // Check if the user exists
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+        if (err) {
+            return res.status(500).json({ msg: err });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ msg: 'User not found!' });
+        }
+
+        const user = result[0];
+
+        // Generate a unique reset token (e.g., a random string)
+        const resetToken = generateResetToken();
+
+        // Store the reset token and its expiration time in the database
+        const resetTokenExpiration = new Date();
+        resetTokenExpiration.setHours(resetTokenExpiration.getHours() + 1); // Token expires in 1 hour
+        db.query('UPDATE users SET reset_token = ?, reset_token_expiration = ? WHERE email = ?', [resetToken, resetTokenExpiration, user.email], (updateErr, updateResult) => {
+            if (updateErr) {
+                return res.status(500).json({ msg: updateErr });
+            }
+
+            // Send the reset token to the user's email (you should implement email sending here)
+            sendResetTokenEmail(email, resetToken);
+
+            return res.json({ msg: 'Password reset token sent to your email' });
+        });
+    });
+});
+
+app.post('/reset-password', (req, res) => {
+    const { email, resetToken, newPassword } = req.body;
+
+    if (!email || !resetToken || !newPassword) {
+        return res.status(400).json({ msg: 'Please enter your email, reset token, and new password' });
+    }
+
+    // Check if the user exists
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+        if (err) {
+            return res.status(500).json({ msg: err });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ msg: 'User not found!' });
+        }
+
+        const user = result[0];
+
+        // Check if the reset token and its expiration time are valid
+        if (user.reset_token !== resetToken || new Date() > user.reset_token_expiration) {
+            return res.status(401).json({ msg: 'Invalid or expired reset token' });
+        }
+
+        // Hash the new password
+        bcrypt.hash(newPassword, 10, (hashErr, hashedPassword) => {
+            if (hashErr) {
+                return res.status(500).json({ msg: hashErr });
+            }
+
+            // Update the user's password and clear the reset token in the database
+            db.query('UPDATE users SET password = ?, reset_token = NULL, reset_token_expiration = NULL WHERE email = ?', [hashedPassword, user.email], (updateErr, updateResult) => {
+                if (updateErr) {
+                    return res.status(500).json({ msg: updateErr });
+                }
+
+                return res.json({ msg: 'Password reset successfully' });
+            });
+        });
+    });
+});
+
+// Placeholder function to send the reset token to the user's email
+function sendResetTokenEmail(email, resetToken) {
+    // Implement your email sending logic here
+    // You might want to use a library like Nodemailer to send emails
+    // Example Nodemailer code:
+    /*
+    const nodemailer = require('nodemailer');
+    const transporter = nodemailer.createTransport({
+        service: 'your-email-service-provider',
+        auth: {
+            user: 'your-email@example.com',
+            pass: 'your-email-password'
+        }
+    });
+
+    const mailOptions = {
+        from: 'your-email@example.com',
+        to: email,
+        subject: 'Password Reset Token',
+        text: `Your password reset token is: ${resetToken}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    */
+   console.log(resetToken)
+}
 
 // login route
 /**
@@ -195,6 +310,13 @@ function authenticateToken(req, res, next) {
         req.user = user;
         next();
     });
+}
+
+// Function to generate a random reset token
+function generateResetToken() {
+    // You can use a library like 'crypto' to generate a secure random token
+    const token = require('crypto').randomBytes(32).toString('hex');
+    return token;
 }
 // start server
 app.listen(3000, () => {
